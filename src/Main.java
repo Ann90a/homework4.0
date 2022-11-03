7ff80e3eb6ec1152a690c7f9b2b53f9caa21146e
Ann90a <abrosimova222@yandex.ru>
2022-11-03 22:34:20 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 123456789;
+        byte b = 113;
+        short c = 27778;
+        long d = -9876544322198L;
+        float f = 1.5F;
+        double j = 2.5;
+        System.out.println("int = " + a);
+        System.out.println("byte = " + b);
+        System.out.println("short = " + c);
+        System.out.println("long = " + d);
+        System.out.println("float = " + f);
+        System.out.println("double = " + j);
+
+
+
+    }
+}
