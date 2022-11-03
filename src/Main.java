6e189bb6e7a046306e300f2735918c03d7b943ad
Ann90a <abrosimova222@yandex.ru>
2022-11-03 23:25:02 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+// задача 1
+
 public class Main {
     public static void main(String[] args) {
         int a = 123456789;
@@ -6,6 +9,7 @@ public class Main {
         long d = -9876544322198L;
         float f = 1.5F;
         double j = 2.5;
+
         System.out.println("int = " + a);
         System.out.println("byte = " + b);
         System.out.println("short = " + c);
@@ -13,6 +17,25 @@ public class Main {
         System.out.println("float = " + f);
         System.out.println("double = " + j);
 
+        // задача 2
+
+        int i1 = 27897;
+        byte b1 = 67;
+        short s1 = -159;
+        long l1 = 987678965549L;
+        float f1 = 27.12F;
+        double d1 = 2.786;
+        char c1 = 569;
+        boolean b2 = false;
+
+        System.out.println(i1);
+        System.out.println(b1);
+        System.out.println(s1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(c1);
+        System.out.println(b2);
 
 
     }
