5e086e885d2069742e5a9799a26a176c23b5832c
Ann90a <abrosimova222@yandex.ru>
2022-11-09 11:12:12 +0300

    Задача 8

--- src/Main.java
+++ src/Main.java
@@ -112,7 +112,30 @@ public class Main {
         double onAverageDays = (double) weightLoss / gramsAverage;
         System.out.print("В среднем на похудение потребуется ");
         System.out.printf("%.1f", onAverageDays);
-        System.out.print(" дней.");
+        System.out.println(" дней.");
+
+        // задача 8
+
+        int masha = 67760;
+        int denis = 83690;
+        int kristina = 76230;
+
+        double percent = 0.1;
+
+        double newSalaryMasha = masha + (masha * percent);
+        double newSalaryDenis = denis + (denis * percent);
+        double newSalaryKristina = kristina + (kristina * percent);
+
+        int year = 12;
+
+        double salaryMashaForYear = (newSalaryMasha * year) - (masha * year) ;
+        double salaryDenisForYear = (newSalaryDenis * year) - (denis * year) ;
+        double salaryKristinaForYear = (newSalaryKristina * year) - (kristina * year) ;
+
+        System.out.println("Маша теперь получает " + newSalaryMasha + " рублей. Годовой доход вырос на " + salaryMashaForYear + " рублей");
+        System.out.println("Денис теперь получает " + newSalaryDenis + " рублей. Годовой доход вырос на " + salaryDenisForYear + " рублей");
+        System.out.println("Кристина теперь получает " + newSalaryKristina + " рублей. Годовой доход вырос на " + salaryKristinaForYear + " рублей");
+
 
         }
     }
